bd0897c5b0e1fc7e3e4aee128db95196cbf89d0e
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-04-01 17:35:56 +0200

    create AppDbContext

--- ProductsApi/Entities/AppDbContext.cs
+++ ProductsApi/Entities/AppDbContext.cs
@@ -1,6 +1,29 @@
-﻿namespace ProductsApi.Entities
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsApi.Entities
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        private readonly string _connectionString =
+          "Server=(localdb)\\mssqllocaldb;Database=Products;Trusted_Connection=True;";
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Description)
+                .HasMaxLength(200);
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+
+        }
     }
 }
