0107d7565baafcbb9146bb0badf2b71bafac027b
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-04-01 19:46:06 +0200

    create product mapping profile

---
+++ ProductsApi/ProductMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductsApi.Entities;
+using ProductsApi.Models;
+
+namespace ProductsApi
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+        }
+    }
+}
