714fb1aea8cf938fea81d3c982238974dded1ff9
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-04-13 12:18:53 +0200

    change create and update validation

--- ProductsApi/Models/CreateProductDto.cs
+++ ProductsApi/Models/CreateProductDto.cs
@@ -4,11 +4,11 @@ namespace ProductsApi.Models
 {
     public class CreateProductDto
     {
-       
-        [Required] public string Name { get; set; }
+
+        public string Name { get; set; }
         public int Number { get; set; }
         public int Quantity { get; set; }
         public string Description { get; set; }
-        [Required] public decimal Price { get; set; }
+        public decimal Price { get; set; }
     }
 }
